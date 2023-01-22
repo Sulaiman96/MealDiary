2d54e185741569b3e5f9ba847ca8f9dcbaa1cd3d
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2023-01-22 17:27:57 +0000

    Cleaned Repo and Git History for fresh start.

---
+++ Backend/MealDiary.Services/Models/Cuisines.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MealDiary.Services.Models;
+
+[Table("Cuisines")]
+public class Cuisines
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+}
---
+++ Backend/MealDiary.Services/Models/Meals.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MealDiary.Services.Models;
+
+[Table("Meals")]
+public class Meals
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public DateTime DateAdded { get; set; }
+    public decimal Price { get; set; }
+    public int Rating { get; set; }
+    public string? Description { get; set; }
+    public string? Review { get; set; }
+    public string MealCourse { get; set; }
+    public DateTime? ShareLinkDate { get; set; }
+    
+    [ForeignKey("Cuisines")]
+    public int? CuisineTypeId { get; set; }
+    [ForeignKey("Users")]
+    public int? UsersId { get; set; }
+    [ForeignKey("Ingredients")]
+    public int? IngredientId { get; set; }
+    [ForeignKey("Images")]
+    public int? ImageId { get; set; }
+}
