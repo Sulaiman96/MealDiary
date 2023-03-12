93b2c1a547e6e8f4133d68ad4384d5247cf249ce
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2023-03-12 22:46:19 +0000

    feat: Client login/register pages setup & working

---
+++ Frontend/Client/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/User').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
