d147ff139b1cab3f6f7c50e6b2b137e112d0c5a1
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2023-03-19 16:12:29 +0000

    feat: routing, routerGuard, new css theme, toast

---
+++ Frontend/Client/src/app/meal-collections/meal-collections.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-collections',
+  templateUrl: './meal-collections.component.html',
+  styleUrls: ['./meal-collections.component.css']
+})
+export class MealCollectionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Frontend/Client/src/app/register/register.component.ts
+++ Frontend/Client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,17 +11,20 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   register() {
     this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error => console.log(error)
+      next: _ => this.cancel(),
+      error: error => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
     })
   }
 
